52814cafdc0a755cfc859d685ab87afca3972b9a
Mohammad Shakib <159904347+Mohammad-Shakib-2004@users.noreply.github.com>
2024-10-24 14:37:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function togglePassword() {
+    const passwordInput = document.getElementById('password');
+    const showPasswordText = document.querySelector('.show-password');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        showPasswordText.textContent = 'Hide';
+    } else {
+        passwordInput.type = 'password';
+        showPasswordText.textContent = 'Show';
+    }
+}
